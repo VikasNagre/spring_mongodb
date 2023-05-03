a1eb5578b09f6b77febee1befcd9126fb5e5c5df
VN170999 <vikasnagre9@gmail.com>
2023-05-03 10:21:14 +0530

    first commit

---
+++ src/main/java/com/example/springMongodb/books/Author.java
@@ -0,0 +1,31 @@
+package com.example.springMongodb.books;
+
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Author {
+	
+	 @Indexed(unique = true)
+	    private String authorName;
+
+	    private int age;
+
+	    public String getAuthorName() {
+	        return authorName;
+	    }
+
+	    public void setAuthorName(String authorName) {
+	        this.authorName = authorName;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) {
+	        this.age = age;
+	    }
+
+}
